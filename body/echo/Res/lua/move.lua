e34b5d891d9f068500d24b0272575cd2bdb7e836
Captain <qq79402005@gmail.com>
2023-05-18 23:56:47 +0800

    New body made by echo #8
    New body made by echo #8

---
+++ body/echo/Res/lua/move.lua
@@ -0,0 +1,34 @@
+local Move = 
+{
+	posX = 10,
+	step = 0.2,
+}
+
+-- start
+function Move:start()
+	--Log:error(type(Log))
+	--Log:error(type(Log.error))
+
+	--for k,v in pairs(getmetatable(Log)) do
+		--Log:error( k .. "->" .. type(v))
+	--end
+	
+end
+
+-- update
+function Move:update()
+	if self.posX > 250 then
+		self.posX = 10
+	end
+	
+	if Input:getMouseButtonDown(0) then
+		self.step = -self.step
+	end
+
+	self.posX = self.posX + self.step
+	self:setPosX(self.posX)
+	
+	self:setEnable(false)
+end
+
+return Move
