ed4ef778ce6f098b1fdce8105beb84111e68d704
Captain <qq79402005@gmail.com>
2023-05-20 02:39:29 +0800

    ehco live2d error fixed #9

--- body/echo/Res/lua/move.lua
+++ body/echo/Res/lua/move.lua
@@ -12,7 +12,6 @@ function Move:start()
 	--for k,v in pairs(getmetatable(Log)) do
 		--Log:error( k .. "->" .. type(v))
 	--end
-	
 end
 
 -- update
@@ -20,15 +19,13 @@ function Move:update()
 	if self.posX > 250 then
 		self.posX = 10
 	end
-	
-	if Input:getMouseButtonDown(0) then
+
+	if Input:isMouseButtonDown(0) then
 		self.step = -self.step
 	end
 
 	self.posX = self.posX + self.step
-	self:setPosX(self.posX)
-	
-	self:setEnable(false)
+	--self:setWorldPositionX(self.posX)
 end
 
 return Move
